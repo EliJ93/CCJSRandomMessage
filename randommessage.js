89603eddb01d4a62a0ac46aef28c9f80c33c3f9d
Elijah Joseph <elijah.joseph93@gmail.com>
2024-11-07 14:52:12 -0800

    Removed stink message

--- randommessage.js
+++ randommessage.js
@@ -2,7 +2,7 @@ const createRandomMessage = () => {
     let messageArray = ['Leeroy Jenkins!', 'At least I have chicken',
         'Pokemon is the best', 'I love my bonshnooks', 'Red and Blue are epic',
         'My car is Sapphire', 'My scooter is Ruby', 'Shadow wadow po dado', 
-        'Cutey pie dog', 'Cooteh pai dog', 'Mr. Bo Jangles', 'Stink buttikers',
+        'Cutey pie dog', 'Cooteh pai dog', 'Mr. Bo Jangles',
         'Petey pop', 'Bonshnookeyookers', 'Doggy woggy po oggy'];
         //console.log(messageArray.length);
         let randomNumber = Math.floor(Math.random() * 14);
